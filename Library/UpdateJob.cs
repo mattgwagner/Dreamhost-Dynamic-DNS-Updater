76278e90307799a3cdf53f0e99a0d3cc6d906266
Matt Wagner <mattgwagner@gmail.com>
2012-12-31 18:07:10 -0500

    Separating service code from library and tests.
    Added configuration items.

---
+++ Library/UpdateJob.cs
@@ -0,0 +1,22 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHDns.Library
+{
+    public class UpdateJob : IJob
+    {
+        public UpdateJob()
+        {
+
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
