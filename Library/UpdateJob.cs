a9d3a9b4d3bed64eace76a5dcf459bd92bca8439
Matt Wagner <mattgwagner@gmail.com>
2012-12-31 18:24:31 -0500

    Added boilerplate code for job, need to actually call the API

--- Library/UpdateJob.cs
+++ Library/UpdateJob.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DHDns.Library
 {
@@ -18,7 +16,43 @@ namespace DHDns.Library
 
         public void Execute(IJobExecutionContext context)
         {
+            // Get existing DNS records
+            var existing = GetDNSRecords(this.config).SingleOrDefault(record => record.Hostname == this.config.Hostname);
+
+            // If there wasn't a matching record, nothing to update
+            // TODO Create record if one doesn't exist?
+            if (existing == null) return;
+
+            // Get current IP
+            var currentIp = GetCurrentIP(this.config);
+
+            // Compare existing record to current IP
+            if (existing.IPAddress != currentIp)
+            {
+                // Update the DNS record
+                UpdateDNSRecord(this.config, currentIp);
+            }
+        }
+
+        public virtual IEnumerable<DnsRecord> GetDNSRecords(IConfig config)
+        {
             throw new NotImplementedException();
+        }
+
+        public virtual String GetCurrentIP(IConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Boolean UpdateDNSRecord(IConfig config, String newIpAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        class DnsRecord
+        {
+            public String Hostname { get; set; }
+            public String IPAddress { get; set; }
         }
     }
 }
