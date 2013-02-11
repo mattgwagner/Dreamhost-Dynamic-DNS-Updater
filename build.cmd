ecfffc0566197997b508df530321f48d369c5c6f
Matt Wagner <mattgwagner@gmail.com>
2013-02-10 20:23:15 -0500

    Added simple build launcher.

---
+++ build.cmd
@@ -0,0 +1,11 @@
+@echo Off
+
+set targets=%1
+
+if "%targets%" == "" (
+	set targets=Build
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "Dreamhost Dynamic DNS Updater.sln" /v:minimal /maxcpucount /target:%targets%
+
+pause
