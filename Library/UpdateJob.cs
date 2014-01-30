23fb60fa32be4a49fda3f1544ecfc4c10dc78f07
Crosmanrond <crosmanrond@gmail.com>
2014-01-30 03:18:31 -0500

    Fixed nested using statements
    the nested using statements could result in the same stream being
    disposed twice.

--- Library/UpdateJob.cs
+++ Library/UpdateJob.cs
@@ -49,8 +49,7 @@ namespace DHDns.Library
 
             var response = request.GetResponse();
 
-            using (var stream = response.GetResponseStream())
-            using (var reader = new StreamReader(stream))
+            using (var reader = new StreamReader(response.GetResponseStream()))
             {
                 return reader.ReadToEnd().Replace("\n", String.Empty);
             }
@@ -126,8 +125,7 @@ namespace DHDns.Library
 
             var response = request.GetResponse();
 
-            using (var stream = response.GetResponseStream())
-            using (var reader = new StreamReader(stream))
+            using (var reader = new StreamReader(response.GetResponseStream()))
             {
                 return reader.ReadToEnd();
             }
