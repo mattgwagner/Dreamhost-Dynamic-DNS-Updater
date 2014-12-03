df484af3bd57ee03744a0e24fcf4744a6c7af6ed
Matt Wagner <mattgwagner@gmail.com>
2014-12-03 13:07:10 -0500

    Fix changed sln name

--- build.cmd
+++ build.cmd
@@ -7,6 +7,6 @@ if "%targets%" == "" (
 	set targets=Build
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "Dreamhost Dynamic DNS Updater.sln" /v:minimal /maxcpucount /target:%targets%
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "Dreamhost_DNS_Updater.sln" /v:minimal /maxcpucount /target:%targets%
 
 pause
